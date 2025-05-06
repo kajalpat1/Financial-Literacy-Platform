b1536ff794f9e71978cb4e7d61aae2168d2060f6
Kajal Patel <158428229+kajalpat1@users.noreply.github.com>
2025-05-05 21:12:47 -0400

    update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express(); //app is a variable that is an instance of express
 const handle = require('./handlers');
-const port = 8000;
+const port = process.env.PORT //to get port from env
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -14,6 +14,7 @@ app.get('/', (req, res) => res.json({hello: 'world' }));
 
 app.use(handle.notFound);
 app.use(handle.errors);
+
 
 
 app.listen(port, console.log(`Server started on port ${port}`)); //start server
