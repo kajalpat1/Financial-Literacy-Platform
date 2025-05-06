87b4ef375d2e3732fba4f2a72250516ee527622a
Kajal Patel <158428229+kajalpat1@users.noreply.github.com>
2025-05-05 21:10:56 -0400

    update

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
+
+require('dotenv').config(); //allows to create private variables dont want to share
+
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express(); //app is a variable that is an instance of express
 const handle = require('./handlers');
 const port = 8000;
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.json({hello: 'world' }));
 
 app.use(handle.notFound);
 app.use(handle.errors);
+
 
 app.listen(port, console.log(`Server started on port ${port}`)); //start server
 //fucntional server, with singular end point and error handle
