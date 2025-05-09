81ce874be0178f7c06a291f9fd826c55533ffe59
Kajal Patel <158428229+kajalpat1@users.noreply.github.com>
2025-05-08 21:11:56 -0400

    update

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,5 +1,17 @@
-import { configureStore } from 'redux';
+import { configureStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import rootReducer from './reducers';
 
-export const store = configureStore();
+const DEFAULT_STATE = {
+    error: {message: null}
+};
+
+export const store = configureStore(
+    rootReducer,
+    DEFAULT_STATE,
+    compose(
+        applyMiddleware(thunk),
+        window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS__EXTENSION__()
+    )
+);
 
