e2ec71f16e67addd2caef9095d598b7247cf67a8
Kajal Patel <158428229+kajalpat1@users.noreply.github.com>
2025-05-14 22:14:32 -0400

    update

--- client/src/containers/RouteViews.jsx
+++ client/src/containers/RouteViews.jsx
@@ -3,42 +3,53 @@ import {Routes, Route} from 'react-router-dom';
 import {connect} from 'react-redux';
 
 import AuthPage from '../pages/AuthPage';
-import TestPage from '../pages/TestPage';
 import HomePage from '../pages/Homepage';
-import { getCurrentPoll } from '../store/actions';
-import PollPage from '../pages/PollPage';
 import CreatePollPage from '../pages/CreatePollPage';
+import PollPage from '../pages/PollPage';
+import TestPage from '../pages/TestPage';
+import { getCurrentPoll } from '../store/actions';
 
-const RouteViews = ({auth, getCurrentPoll}) => (
-<main> 
+const RouteViews = ({ auth, getCurrentPoll }) => (
+  <main>
     <Routes>
-        <Route path="/" element={<HomePage />} />
-    
-    <Route 
-    path="/login" 
-    element={<AuthPage 
-    authType="login" i
-    sAuthenticated={auth.isAuthenticated} />} />
-    
-    <Route 
-    path="/register" 
-    element={<AuthPage a
-    uthType="register" 
-    isAuthenticated={auth.isAuthenticated} />} />
+      <Route path="/" element={<HomePage />} />
 
-<Route path="/poll/new" element={
-    <CreatePollPage isAuthenticated={auth.isAuthenticated} />} />
+      <Route
+        path="/login"
+        element={
+          <AuthPage
+            authType="login"
+            isAuthenticated={auth.isAuthenticated}
+          />
+        }
+      />
 
-    <Route path="/poll/:id" element={<PollPage />} />
+      <Route
+        path="/register"
+        element={
+          <AuthPage
+            authType="register"
+            isAuthenticated={auth.isAuthenticated}
+          />
+        }
+      />
 
-    <Route path="/test" element={<TestPage />} />
-   
+      <Route
+        path="/poll/new"
+        element={<CreatePollPage isAuthenticated={auth.isAuthenticated} />}
+      />
+
+      <Route
+        path="/poll/:id"
+        element={<PollPage getPoll={(id) => getCurrentPoll(id)} />}
+      />
+
+      <Route path="/test" element={<TestPage />} />
     </Routes>
+  </main>
+);
 
-
-</main>);
-
-//props? for poll page and home page
-
-
-export default connect(store => ({auth: store.auth}), {getCurrentPoll})(RouteViews);
+export default connect(
+  (store) => ({ auth: store.auth }),
+  { getCurrentPoll }
+)(RouteViews);
