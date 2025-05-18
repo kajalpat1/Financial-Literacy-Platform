57d02639185b33c6bed072be0c9800e1ec1fd3c5
Kajal Patel <158428229+kajalpat1@users.noreply.github.com>
2025-05-17 21:23:37 -0400

    update

--- client/src/containers/RouteViews.jsx
+++ client/src/containers/RouteViews.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Routes, Route} from 'react-router-dom';
 import {connect} from 'react-redux';
+import { logout } from '../store/actions';
 
 
 import AuthPage from '../pages/AuthPage';
