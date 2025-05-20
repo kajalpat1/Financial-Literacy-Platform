75ffabbae095a49bb6a9c1ed2c408cf5addc6e37
Kajal Patel <158428229+kajalpat1@users.noreply.github.com>
2025-05-19 22:45:42 -0400

    few fixes with financial

--- client/src/components/BudgetScenario.jsx
+++ client/src/components/BudgetScenario.jsx
@@ -1,6 +1,19 @@
-// BudgetScenario.jsx (New Component)
+
 import React, { useState } from 'react';
 import { Line } from 'react-chartjs-2';
+
+import {
+  Chart as ChartJS,
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  Tooltip,
+  Legend
+} from 'chart.js';
+
+ChartJS.register(LineElement, CategoryScale, LinearScale, PointElement, Tooltip, Legend);
+
 
 const compoundInterest = (initial, rate, years) => {
   const values = [];
